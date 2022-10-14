f095a332cd8bcdceba524674318dc7df0d26a053
Leandro Vieira Reis <110622024+criacaolvr@users.noreply.github.com>
2022-10-14 15:34:02 -0300

    autenticar

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
+from crypt import methods
 from app import app
 from flask import render_template
+from flask import request
 
 @app.route('/')
 @app.route('/index')
@@ -11,3 +13,9 @@ def index():
 @app.route('/usuario')
 def usuario():
     return render_template('usuario.html')
+
+@app.route('/autenticar', methods=['GET'])
+def autenticar():
+    usuario = request.args.get('usuario')
+    senha = request.args.get('senha')
+    return "Usuario:{} e Senha {}".format(usuario, senha)
